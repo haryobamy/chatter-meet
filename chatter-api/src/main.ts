8dbb0b1cc565c0ee6a368430dadd67878672b467
haryobamy <haryobamy.badmus@gmail.com>
2023-06-30 17:04:32 +0100

    bug d=fixes

--- chatter-api/src/main.ts
+++ chatter-api/src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { json } from 'express';
+import cors from 'cors';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.use(json({ limit: '25mb' }));
-  app.enableCors();
+  // app.enableCors();
   await app.listen(4200);
 }
 bootstrap();
