7174761ff0fa3f8f4a6115f9b00a2c420e7966b2
haryobamy <haryobamy.badmus@gmail.com>
2023-06-30 17:39:14 +0100

    update

--- chatter-api/src/main.ts
+++ chatter-api/src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.use(json({ limit: '25mb' }));
-  // app.enableCors();
+  app.enableCors();
   await app.listen(4200);
 }
 bootstrap();
